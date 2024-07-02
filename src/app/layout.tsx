a49171d5be3c4c243d046bed514eb550e1205d8d
Kevin Zoltany <kzoltany@gmail.com>
2024-07-02 12:02:29 -0700

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import '~/styles/globals.css'
+
+import { GeistSans } from 'geist/font/sans'
+
+export const metadata = {
+    title: 'CNLA',
+    description: 'Code Ninjas Northridge & Los Angeles',
+    icons: [{ rel: 'icon', url: '/favicon.ico' }]
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en" className={`${GeistSans.variable}`}>
+            <body>{children}</body>
+        </html>
+    )
+}
